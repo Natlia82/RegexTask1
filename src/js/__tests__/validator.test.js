a05a66969b1770143d262dfd2dbd57db92f64c93
Natalia <nata@host>
2021-05-30 14:33:43 +0500

    Изменила имена и добавила тесты с "-" и "_"

--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -1,19 +1,29 @@
 import Validator from '../validator';
 
-test('valid1', () => {
+test('validRight', () => {
     const result = Validator.validateUsername('2fgl3b4');
     expect(result).toBe(true);
 });
 
 
-test('valid2', () => {
+test('repeatedNumbers', () => {
     const result = Validator.validateUsername('2b77772');
 
     expect(result).toBe(false);
 });
 
-test('valid3', () => {
+test('notTheFirstNumber', () => {
     const result = Validator.validateUsername('b2fgl3b4');
 
     expect(result).toBe(false);
+});
+
+test('containsAnUnderscore', () => {
+    const result = Validator.validateUsername('_2fg_l3b4');
+    expect(result).toBe(true);
+});
+
+test('containsAHyphen', () => {
+    const result = Validator.validateUsername('2fgl3b4-');
+    expect(result).toBe(true);
 });
