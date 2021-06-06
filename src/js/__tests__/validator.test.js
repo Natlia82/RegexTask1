21468a69f3465705e4d14043b85ce75123eabc54
Natalia <nata@host>
2021-06-06 10:51:12 +0500

    Исправила рег. выражение и тесты

--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -1,29 +1,29 @@
 import Validator from '../validator';
 
 test('validRight', () => {
-    const result = Validator.validateUsername('2fgl3b4');
+    const result = Validator.validateUsername('test123test');
     expect(result).toBe(true);
 });
 
 
 test('repeatedNumbers', () => {
-    const result = Validator.validateUsername('2b77772');
+    const result = Validator.validateUsername('1test2');
 
     expect(result).toBe(false);
 });
 
 test('notTheFirstNumber', () => {
-    const result = Validator.validateUsername('b2fgl3b4');
+    const result = Validator.validateUsername('_test');
 
     expect(result).toBe(false);
 });
 
 test('containsAnUnderscore', () => {
-    const result = Validator.validateUsername('_2fg_l3b4');
+    const result = Validator.validateUsername('te_st');
     expect(result).toBe(true);
 });
 
 test('containsAHyphen', () => {
-    const result = Validator.validateUsername('2fgl3b4-');
-    expect(result).toBe(true);
+    const result = Validator.validateUsername('test-');
+    expect(result).toBe(false);
 });
--- src/js/validator.js
+++ src/js/validator.js
@@ -1,7 +1,7 @@
 export default class Validator {
     static validateUsername(name) {
-        return /^[\d_-](\d{1,3}[a-zA-Z_-]+|[a-zA-Z_-]*|[a-zA-Z_-]+\d{1,3})*[\d_-]$/.test(name);
-
+        //  return /^[\d_-](\d{1,3}[a-zA-Z_-]+|[a-zA-Z_-]*|[a-zA-Z_-]+\d{1,3})*[\d_-]$/.test(name);
+        return /^[a-zA-Z](\d{1,3}[a-zA-Z_-]+|[a-zA-Z_-]*|[a-zA-Z_-]+\d{1,3})*[a-zA-Z]$/.test(name);
     }
 
 }
